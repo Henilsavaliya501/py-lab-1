9a25196456f489b9e90d598c0e11bc8441a8d5d4
Henilsavaliya501 <henilsavaliya500@gmail.com>
2026-01-12 15:22:40 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,19 @@
+def all():
+    
+    a = int(input("enter a no :"))
+    b = int(input("enter a another no :"))
+    c1=a+b
+    c2=a-b
+    c3=a*b
+    c4=a/b
+    print(a, "+",b,"=",c1)
+    print(a, "-",b,"=",c2)
+    print(a, "*",b,"=",c3)
+    print(a, "/",b,"=",c4)    
+#    print()
+    print("answer =",c1)
+    print("answer =",c2)
+    print("answer =",c3)
+    print("answer =",c4)
+
+all()
