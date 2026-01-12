e178cb8fd533e261d86ded9e6c6e44507caa5ead
Henilsavaliya501 <henilsavaliya500@gmail.com>
2026-01-12 15:10:35 +0530

    Add division function to calculate and display result

---
+++ 4.py
@@ -0,0 +1,10 @@
+def div():
+    
+    a = int(input("enter a no :"))
+    b = int(input("enter a another no :"))
+    c=a/b
+    print(a, "/",b,"=",c)
+#    print()
+    print("answer =",c)
+
+div()
