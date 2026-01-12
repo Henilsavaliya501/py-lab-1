4029f580423d4baaa94e208eb95437a14a1b5c57
Henilsavaliya501 <henilsavaliya500@gmail.com>
2026-01-12 15:08:44 +0530

    Add multiplication function to calculate product

---
+++ 3.py
@@ -0,0 +1,10 @@
+def mul():
+    
+    a = int(input("enter a no :"))
+    b = int(input("enter a another no :"))
+    c=a*b
+    print(a, "*",b,"=",c)
+#    print()
+    print("answer =",c)
+
+mul()
